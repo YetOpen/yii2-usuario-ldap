fe7b834b6a5e7cc0db713aad4f4bddbc112d30d9
marco.piazza <marco.piazza@yetopen.it>
2018-10-02 16:08:54 +0200

    Prima stesura estensione

---
+++ Module.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace yetopen\usuario_ldap;
+
+use Yii;
+use yii\base\Model as BaseModule;
+use yii\helpers\ArrayHelper;
+use adLDAP\adLDAPException;
+//use \Adldap\Configuration\DomainConfiguration;
+use adLDAP\adLDAP as Adldap;
+use yii\base\Event;
+use app\controllers\user\SecurityController;
+use Da\User\Event\FormEvent;
+use Da\User\Model\User;
+
+class Module extends BaseModule
+{
+    public static $ldapProvider;
+
+    public $ldapConfig;
+
+    public $accountSuffix;
+
+    public $enableLdapUpdate;
+
+    public function init()
+    {
+        $ad = new Adldap($this->ldapConfig);
+        $ad->setAccountSuffix($this->accountSuffix);
+        try {
+            $ad->connect();
+            self::$ldapProvider = $ad;
+        } catch (adLDAPException $e) {
+            var_dump($e);
+            die;
+        }
+        $this->events();
+        parent::init();
+    }
+
+    public function events() {
+        Event::on(SecurityController::class, FormEvent::EVENT_BEFORE_LOGIN, function (FormEvent $event) {
+            $provider = Yii::$app->usuarioLdap::$ldapProvider;
+            $form = $event->getForm();
+
+            $username = $form->login;
+            $password = $form->password;
+            if(!$provider->authenticate($username, $password)) {
+                return;
+            }
+
+            $userIdentity = User::findIdentity(User::findOne(['username' => $username])->id);
+            if(empty($userIdentity)) {
+                $userIdentity = new User();
+                $userIdentity->username       = $username;
+                $userIdentity->password       = $password;
+                $userIdentity->email          = $username."@ldap.com"; //FIXME prendere l'email da utente LDAP
+                $userIdentity->confirmed_at   = time();
+                $userIdentity->blocked_at     = time();
+            }
+            $duration = $form->rememberMe ? $form->module->rememberLoginLifespan : 0;
+
+            return Yii::$app->getUser()->login($userIdentity, $duration);
+
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+Yii2 Usuario Ldap
+=================
+An yii2 extension to syncronize LDAP users in Yii
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist yetopen/yii2-usuario-ldap "*"
+```
+
+or add
+
+```
+"yetopen/yii2-usuario-ldap": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
