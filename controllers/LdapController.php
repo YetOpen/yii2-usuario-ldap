1d1c6ebdbcad15339785c2dca4f1fc32045f0203
andrea.scaramucci <andrea.scaramucci@yetopen.com>
2024-03-29 10:37:03 +0100

    Improved search action

--- controllers/LdapController.php
+++ controllers/LdapController.php
@@ -2,7 +2,10 @@
 
 namespace yetopen\usuarioLdap\controllers;
 
+use Adldap\Models\Contact;
+use Adldap\Models\User;
 use Da\User\Filter\AccessRuleFilter;
+use Da\User\Model\User as UsuarioUser;
 use yetopen\helpers\controllers\YOController;
 use yetopen\usuarioLdap\UsuarioLdapComponent;
 use Yii;
@@ -36,17 +39,19 @@ class LdapController extends YOController
         $ldapComponent = Yii::$app->usuarioLdap;
         $ldapUsers = $ldapComponent->findLdapUsers($q);
         $output = [];
-        foreach ($ldapUsers as $d) {
-            if(!empty($d->mail)) {
-                $mail = $d->mail[0];
-            } else {
-                // If there is, I show the username found, else show the search string
-                $username = !empty($d->samaccountname) ? $d->samaccountname[0] : $q;
-                $mail = $username . ' - ' . Yii::t('usuarioLdap', 'user without mail');
+        foreach ($ldapUsers as $ldapUser) {
+            /** @var User|Contact $ldapUser */
+            $userData = [];
+            $value = is_a(User::class, $ldapUser) ? $ldapUser->getAuthIdentifier() : $ldapUser->getEmail();
+            $userData['value'] = $value;
+            $userData['label'] = $ldapUser->getFirstName() . ' ' . $ldapUser->getLastName() . ' - ' . $ldapUser->getEmail();
+            $userData['q'] = $q;
+            if(empty($value)) {
+                // User has neither uid nor email, showing it with note that it has no mail
+                $userData['value'] = UsuarioUser::LDAP_INVALID_USER;
+                $userData['label'] = $userData['label'] . Yii::t('usuario', 'Invalid LDAP user');
             }
-            $output[] = [
-                'value' => $mail
-            ];
+            $output[] = $userData;
         }
         return $output;
     }
